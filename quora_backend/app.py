476219e00729d771c048eb1282ffdf5d98519e7d
abhilash-m96 <abhilashmohanram@gmail.com>
2020-12-16 16:34:41 +0530

    added jwt_extended for user login

--- quora_backend/app.py
+++ quora_backend/app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask_restful import Api
+from flask_jwt_extended import JWTManager, get_jwt_claims
 
-from resources.user import UserResource, UserByUsername, UserList
+from resources.user import UserResource, UserByUsername, UserList, UserLogin
 from resources.question import QuestionResource, QuestionList
 from resources.answer import AnswerResource, AnswerList
 from resources.comment import CommentResource, CommentList
@@ -12,6 +13,26 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["PROPAGATE_EXCEPTIONS"] = True
 app.secret_key = "abhi"
 api = Api(app)
+
+jwt = JWTManager(app)
+
+"""
+@jwt.user_claims_loader
+def add_claims_to_jwt(identity):
+    if identity == 1:  # first user
+        return {"is_admin": True}
+    return {"is_admin": False}
+"""
+# use the above for delete operations in resources
+# as shown below
+
+"""
+@jwt_required
+def delete(self, name):
+    claims = get_jwt_claims()
+    if not claims.get("is_admin"):
+        return {"message": "Admin privilege required"}, 401
+"""
 
 
 @app.before_first_request
@@ -28,6 +49,7 @@ api.add_resource(AnswerResource, "/answer")
 api.add_resource(AnswerList, "/answers")
 api.add_resource(CommentResource, "/comment")
 api.add_resource(CommentList, "/comments")
+api.add_resource(UserLogin, "/login")
 
 if __name__ == "__main__":
     from db import db
--- quora_backend/resources/user.py
+++ quora_backend/resources/user.py
@@ -1,5 +1,7 @@
 from flask_restful import Resource, reqparse
 from models.user import UserModel
+from flask_jwt_extended import create_access_token, create_refresh_token, jwt_required
+from werkzeug.security import safe_str_cmp
 
 
 class UserResource(Resource):
@@ -45,6 +47,30 @@ class UserByUsername(Resource):
         return {"message": "User not found"}, 404
 
 
+class UserLogin(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        "username", type=str, required=True, help="This field cannot be blank."
+    )
+    parser.add_argument(
+        "password", type=str, required=True, help="This field cannot be blank."
+    )
+
+    @classmethod
+    def post(cls):
+        data = cls.parser.parse_args()
+
+        user = UserModel.find_by_username(data["username"])
+        if user and safe_str_cmp(user.password, data["password"]):
+            access_token = create_access_token(identity=user.id, fresh=True)
+            refresh_token = create_refresh_token(user.id)
+
+            return {"access_token": access_token, "refresh_token": refresh_token}, 200
+
+        return {"message": "Invalid credentials"}, 401
+
+
 class UserList(Resource):
+    @jwt_required
     def get(self):
         return {"users": list(map(lambda x: x.json(), UserModel.query.all()))}
